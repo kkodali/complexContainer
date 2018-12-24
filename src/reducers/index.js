c4b013c841d9d892fca97fe1e529e07677e3f3f4
kkodali <kodalikarthik@yahoo.com>
2018-12-24 14:35:21 -0500

    Fix merge issue

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 // import {combineReducers} from 'redux';
 import step from './reducerStep';
 
 const rootReducer = step;
 
-=======
-// import {combineReducers} from 'redux';
-import step from './reducerStep';
-
-const rootReducer = step;
-
->>>>>>> ac9b403bd212644d3a86ebbf35eff29c408d9e87
 export default rootReducer;
